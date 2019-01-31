0dfeb451a32e44ec0a002d9b01c76da5daf4573d
anil oggu <aniloggu@anils-MacBook-Pro.local>
2019-01-30 22:43:23 -0600

    new branch created

--- Reactive Form/reactiveform/README.md
+++ Reactive Form/reactiveform/README.md
@@ -1,4 +1,4 @@
-# App
+# Reactiveform
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- Reactive Form/reactiveform/src/app/app.component.spec.ts
+++ Reactive Form/reactiveform/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, HomeComponent
       ],
     }).compileComponents();
   }));
@@ -22,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to App!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to reactiveform!');
   }));
 });
